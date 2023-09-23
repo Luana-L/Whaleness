9eac8048b9bbd47a1491766074d8a83767de5346
Luana-L <luana.liao.6@gmail.com>
2023-09-23 13:49:28 -0400

    Initial Commit

---
+++ Whaleness/WhalenessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhalenessApp.swift
+//  Whaleness
+//
+//  Created by Luana Liao on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WhalenessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
