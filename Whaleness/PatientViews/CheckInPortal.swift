007146ff261a78b860dd2d428840ecd820055b54
Sarah Hong <ssh2198@columbia.edu>
2023-09-24 10:56:49 -0400

    checkin portal updates

--- Whaleness/PatientViews/CheckInPortal.swift
+++ Whaleness/PatientViews/CheckInPortal.swift
@@ -1,6 +1,15 @@
 //
 import SwiftUI
 
+
+
+struct CheckInInstance {
+    var symptom: Symptom
+    var med: Med
+    var date: Date
+    var pain: Int
+}
+
 struct Symptom {
     var name: String
     var isChecked: Bool = false
@@ -24,6 +33,8 @@ var timeFormatter: DateFormatter {
 }
 
 struct CheckInPortal: View {
+    @State private var checkInList: [Symptom] = []
+    
     @State private var painLevel = 0
     @State private var symptoms = [ Symptom(name: "Fever"), Symptom(name: "Nausea"), Symptom(name: "Slurred speech"), Symptom(name: "Muscle pain"),
         Symptom(name: "")
@@ -47,163 +58,167 @@ struct CheckInPortal: View {
                 }
                 .padding(.leading, 20)
                 .padding(.vertical, 10)
-                Text("Pain Level")
-                    .padding(.leading, 20)
-                    .font(Font.custom("Avenir", size: 20))
-                HStack(spacing: 20) {
-                    Spacer()
-                    Button {painLevel = 5} label: {
-                        Image("pain5")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40)
-                            .background(painLevel == 5 ? Color.cyan : Color.white)
-                            .clipShape(Circle())
-                    }
-                    Button {painLevel = 4} label: {
-                        Image("pain4")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40)
-                            .background(painLevel == 4 ? Color.cyan : Color.white)
-                            .clipShape(Circle())
-                    }
-                    Button {painLevel = 3} label: {
-                        Image("pain3")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40)
-                            .background(painLevel == 3 ? Color.cyan : Color.white)
-                            .clipShape(Circle())
-                    }
-                    Button {painLevel = 2} label: {
-                        Image("pain2")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40)
-                            .background(painLevel == 2 ? Color.cyan : Color.white)
-                            .clipShape(Circle())
-                    }
-                    Button {painLevel = 1} label: {
-                        Image("pain1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40)
-                            .background(painLevel == 1 ? Color.cyan : Color.white)
-                            .clipShape(Circle())
-                    }
-                    Spacer()
-                }
-                .padding(.horizontal, 20)
                 
-                Text("Symptoms")
-                    .padding(.leading, 20)
-                    .font(Font.custom("Avenir", size: 20))
-                HStack(alignment: .top, spacing: 10) { // not padded??
-                    VStack(alignment: .leading, spacing: 10) {
-                        ForEach(symptoms.indices, id: \.self) { index in
-                            HStack(alignment: .top, spacing: 10) {
-                                Toggle(isOn: $symptoms[index].isChecked) {
-                                    Text(symptoms[index].name)
-                                        .padding(5)
-                                    if index == symptoms.count - 1 {
-                                        TextField("Add custom symptom", text: $newSymptom)
-                                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                                            .font(Font.custom("Avenir", size: 20))
-                                        Spacer()
-                                        
-                                        Button(action: {
-                                            if !newSymptom.isEmpty {
-                                                let insertIndex = symptoms.count - 1
-                                                symptoms.insert(Symptom(name: newSymptom, isChecked: true), at: insertIndex)
-                                                newSymptom = ""
+                ScrollView{
+                    Text("Pain Level")
+                        .padding(.leading, 20)
+                        .font(Font.custom("Avenir", size: 20))
+                    HStack(spacing: 20) {
+                        Spacer()
+                        Button {painLevel = 5} label: {
+                            Image("pain5")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40)
+                                .background(painLevel == 5 ? Color.cyan : Color.white)
+                                .clipShape(Circle())
+                        }
+                        Button {painLevel = 4} label: {
+                            Image("pain4")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40)
+                                .background(painLevel == 4 ? Color.cyan : Color.white)
+                                .clipShape(Circle())
+                        }
+                        Button {painLevel = 3} label: {
+                            Image("pain3")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40)
+                                .background(painLevel == 3 ? Color.cyan : Color.white)
+                                .clipShape(Circle())
+                        }
+                        Button {painLevel = 2} label: {
+                            Image("pain2")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40)
+                                .background(painLevel == 2 ? Color.cyan : Color.white)
+                                .clipShape(Circle())
+                        }
+                        Button {painLevel = 1} label: {
+                            Image("pain1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40)
+                                .background(painLevel == 1 ? Color.cyan : Color.white)
+                                .clipShape(Circle())
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Text("Symptoms")
+                        .padding(.leading, 20)
+                        .font(Font.custom("Avenir", size: 20))
+                    HStack(alignment: .top, spacing: 10) { // not padded??
+                        VStack(alignment: .leading, spacing: 10) {
+                            ForEach(symptoms.indices, id: \.self) { index in
+                                HStack(alignment: .top, spacing: 10) {
+                                    Toggle(isOn: $symptoms[index].isChecked) {
+                                        Text(symptoms[index].name)
+                                            .padding(5)
+                                        if index == symptoms.count - 1 {
+                                            TextField("Add custom symptom", text: $newSymptom)
+                                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                                .font(Font.custom("Avenir", size: 20))
+                                            Spacer()
+                                            
+                                            Button(action: {
+                                                if !newSymptom.isEmpty {
+                                                    let insertIndex = symptoms.count - 1
+                                                    symptoms.insert(Symptom(name: newSymptom, isChecked: true), at: insertIndex)
+                                                    newSymptom = ""
+                                                }
+                                            }) {
+                                                Image(systemName: "plus.circle.fill")
+                                                    .foregroundColor(.blue)
+                                                    .font(.system(size: 20))
                                             }
-                                        }) {
-                                            Image(systemName: "plus.circle.fill")
-                                                .foregroundColor(.blue)
-                                                .font(.system(size: 20))
-                                        }
-                                    } else {
-                                        Spacer()
-                                        Button(action: {
-                                            symptoms.remove(at: index)
-                                        }) {
-                                            Image(systemName: "minus.circle.fill")
-                                                .foregroundColor(.red)
-                                                .font(.system(size: 20))
+                                        } else {
+                                            Spacer()
+                                            Button(action: {
+                                                symptoms.remove(at: index)
+                                            }) {
+                                                Image(systemName: "minus.circle.fill")
+                                                    .foregroundColor(.red)
+                                                    .font(.system(size: 20))
+                                            }
                                         }
                                     }
+                                    .toggleStyle(CheckboxToggleStyle())
+                                    
+                                    
+                                    
                                 }
-                                .toggleStyle(CheckboxToggleStyle())
-                                
-                                
-                                
+                                .foregroundStyle(Color("DarkGrey"))
                             }
-                            .foregroundStyle(Color("DarkGrey"))
+                            
                         }
-                        
+                        .padding(5)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
-                    .padding(5)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                .padding(.vertical, 10)
-                .padding(.horizontal, 20)
-                .background(Color("LightGrey"))
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                
-                Text("Medication")
+                    .padding(.vertical, 10)
                     .padding(.horizontal, 20)
-                    .font(Font.custom("Avenir", size: 20))
-                
-                HStack(alignment: .top, spacing: 10) {
+                    .background(Color("LightGrey"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
                     
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("\(day, formatter: dateFormatter)")
-                            .font(Font.custom("Avenir", size:15))
+                    Text("Medication")
+                        .padding(.horizontal, 20)
+                        .font(Font.custom("Avenir", size: 20))
+                    
+                    HStack(alignment: .top, spacing: 10) {
                         
-                        ForEach(medicines.indices, id: \.self) { index in
-                            HStack(alignment: .top, spacing: 10) {
-                                Toggle(isOn: $medicines[index].isChecked) {
-                                    Text(medicines[index].name)
-                                        .padding(5)
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("\(day, formatter: dateFormatter)")
+                                .font(Font.custom("Avenir", size:15))
+                            
+                            ForEach(medicines.indices, id: \.self) { index in
+                                HStack(alignment: .top, spacing: 10) {
+                                    Toggle(isOn: $medicines[index].isChecked) {
+                                        Text(medicines[index].name)
+                                            .padding(5)
+                                    }
+                                    .toggleStyle(CheckboxToggleStyle())
+                                    
                                 }
-                                .toggleStyle(CheckboxToggleStyle())
-                                
+                                .foregroundStyle(Color("DarkGrey"))
                             }
-                            .foregroundStyle(Color("DarkGrey"))
+                            
+                            HStack {
+                                Spacer()
+                                Text("View past appointment records")
+                                    .font(Font.custom("Avenir", size: 18))
+                                    .foregroundStyle(Color.blue)
+                            }
                         }
-                        
-                        HStack {
-                            Spacer()
-                            Text("View past appointment records")
-                                .font(Font.custom("Avenir", size: 18))
-                                .foregroundStyle(Color.blue)
+                        .padding(5)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 20)
+                    .background(Color("LightGrey"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    
+                    NavigationLink(destination: CheckInSubmit()) {
+                        Button {
+                            print("Success")
+                        } label: {
+                            Text("Submit")
+                                .padding(.horizontal)
+                                .foregroundColor(.white)
+                                .background(Color("DarkBlue"))
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .font(Font.custom("Avenir", size:15))
+                            
                         }
                     }
-                    .padding(5)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                .padding(.vertical, 10)
-                .padding(.horizontal, 20)
-                .background(Color("LightGrey"))
-                .clipShape(RoundedRectangle(cornerRadius: 20))
                     
                 }
-            
-            
-            NavigationLink(destination: CheckInSubmit()) {
-                Button {} label: {
-                    Text("Submit")
-                        .padding(.horizontal)
-                        .foregroundColor(.white)
-                        .background(Color("DarkBlue"))
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                        .font(Font.custom("Avenir", size:15))
-
-                }
+                
             }
-                       
-            
             
         }
 
