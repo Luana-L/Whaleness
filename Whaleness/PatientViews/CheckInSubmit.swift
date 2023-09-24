b7f2a5d85aaa0faac4c6e55979abe29797ac97bc
Jenny Ye <jy3056@columbia.edu>
2023-09-24 11:03:10 -0400

    checkin submit page updates

--- Whaleness/PatientViews/CheckInSubmit.swift
+++ Whaleness/PatientViews/CheckInSubmit.swift
@@ -11,25 +11,30 @@ struct CheckInSubmit: View {
     var body: some View {
         NavigationView{
             
-            VStack {
+            VStack (alignment: .leading){
+                Image("dark-background-finley")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top, 30)
+                
                 HStack{
                     Text("Thank you!")
                         .font(Font.custom("Avenir", size: 30))
                         .foregroundStyle(Color("DarkBlue"))
                     Spacer()
                 }
-                        NavigationLink(destination: CheckInPortal()) {
-                            Text("Submit another CheckIn?")
-                                .font(Font.custom("Avenir", size: 18))
-                                .foregroundStyle(Color.blue)
-                        }
-                        
-                        NavigationLink(destination: ChatPortal()) {
-                            Text("Talk to someone?")
-                                .font(Font.custom("Avenir", size: 18))
-                                .foregroundStyle(Color.blue)
-                        }
-                    }
+                NavigationLink(destination: CheckInPortal()) {
+                    Text("Submit another CheckIn?")
+                        .font(Font.custom("Avenir", size: 18))
+                        .foregroundStyle(Color.blue)
+                }
+                
+                NavigationLink(destination: ChatPortal()) {
+                    Text("Talk to someone?")
+                        .font(Font.custom("Avenir", size: 18))
+                        .foregroundStyle(Color.blue)
+                }
+            }
             
             
             .padding(.leading, 20)
